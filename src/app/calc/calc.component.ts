053c2c2926caf660ef45f321c1670d6389716606
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-03-01 19:51:56 -0300

    commit 1

---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+    selector:'app-calc',
+    templateUrl:'./calc.component.html',
+    
+})
+export class CalcComponent{
+
+    soma(){
+        
+    }
+    
+
+
+
+}
